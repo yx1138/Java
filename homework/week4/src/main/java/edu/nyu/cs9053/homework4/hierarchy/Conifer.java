b870aa35bed751e174983ab8b3656cc14edfcf00
ZengJing <Jing@ZengJings-MacBook-Air.local>
2016-10-05 12:55:22 -0400

    * Homework x submission

--- homework/week4/src/main/java/edu/nyu/cs9053/homework4/hierarchy/Conifer.java
+++ homework/week4/src/main/java/edu/nyu/cs9053/homework4/hierarchy/Conifer.java
@@ -6,4 +6,20 @@ public abstract class Conifer extends Tree{
 	public Conifer(String name, Type type,int age){
 		super(name,type,age,new Gymnosperm());
 	}
+	public boolean equals(Object o){
+		if (this == o){
+			return true;
+		}
+		if(o == null || getClass() != o.getClass()){
+			return false;
+		}
+		if ( !super.equals(o)){
+			return false;
+		}
+		return true;
+	}
+	public  int hashCode(){
+		int result = super.hashCode();
+		return result;
+	}
 }
