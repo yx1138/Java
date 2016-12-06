f5db95bc8c2b8201c45aa1ce68756f140ac6397f
ZengJing <Jing@ZengJings-MacBook-Air.local>
2016-12-06 00:51:20 -0500

    * Homework 11 submission

--- homework/week11/src/main/java/edu/nyu/cs9053/homework11/BlockingChatter.java
+++ homework/week11/src/main/java/edu/nyu/cs9053/homework11/BlockingChatter.java
@@ -1,20 +1,79 @@
 package edu.nyu.cs9053.homework11;
 
 import java.io.*;
-
+import java.util.Random;
+import java.nio.charset.Charset;
 /**
  * User: blangel
  * Date: 11/23/14
  * Time: 4:31 PM
  */
 public class BlockingChatter implements Chatter {
-
+	private static final Charset UTF8 = Charset.forName("UTF-8");
+	private static final int READ_BUFFER_SIZE = 1024;
+	private static final int MAX_LINE = 21691;
+	private final InputStream chatServerInput;
+	private final OutputStream chatServerOutput;
+	private final InputStream userInput;
     public BlockingChatter(InputStream chatServerInput, OutputStream chatServerOutput, InputStream userInput) {
         // TODO
+        this.chatServerInput = chatServerInput;
+        this.chatServerOutput = chatServerOutput;
+        this.userInput = userInput;
+
     }
 
     @Override public void run() {
-        // TODO
+    	Thread threadForInput = new Thread(new Runnable(){
+    		public void run(){
+    			while(true){
+    				byte[] buffer = new byte[READ_BUFFER_SIZE];
+        			try{
+        			int length = userInput.read(buffer);
+    				chatServerOutput.write(buffer, 0, length);
+        			}catch (IOException e){
+						System.out.printf("Exception - %s%n", e.getMessage());
+        			}
+    			}
+    		}
+    	});
+    	Thread threadForServer = new Thread(new Runnable(){
+    		public void run(){
+    			while(true){
+        			byte[] buffer = new byte[READ_BUFFER_SIZE];
+        			try{
+        				int len = chatServerInput.read(buffer);
+						String messageFormServer = new String(buffer);
+						System.out.println(messageFormServer);
+						String easterEggFlag = messageFormServer.substring(19,23);
+						if(easterEggFlag.equals("java")){
+							EasterEgg();
+						}
+        			}catch (IOException e){
+        				System.out.printf("Exception - %s%n", e.getMessage());
+        			}			
+        		}
+    		}
+    	});
+    	threadForServer.start();
+    	threadForInput.start();
+        
     }
+    public void EasterEgg() throws IOException {
+    	String file = "src/main/resources/Moby Dick.txt";
+    	Random random = new Random();
+    	int random_line_number = random.nextInt(MAX_LINE);
+    	FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+    	String line = reader.readLine();
+		for (int i = 0; i < random_line_number + 1; i++) {
+  			line = reader.readLine();
+		}
+		if(line.equals("")){
+			line=" ";
+		}
+		byte[] buffer = line.getBytes(UTF8);
+		chatServerOutput.write(buffer, 0, buffer.length);
 
+    }
 }
--- homework/week11/src/main/java/edu/nyu/cs9053/homework11/NonBlockingChatter.java
+++ homework/week11/src/main/java/edu/nyu/cs9053/homework11/NonBlockingChatter.java
@@ -15,14 +15,54 @@ import java.util.Set;
  * Time: 4:32 PM
  */
 public class NonBlockingChatter implements Chatter {
+	private static final Charset UTF8 = Charset.forName("UTF-8");
 
+	private static final int READ_BUFFER_SIZE = 1024;
+
+	private final SocketChannel chatServerChannel;
+
+    private final ByteBuffer readBuffer;
+
+    private final ByteBuffer writeBuffer;
+
+    private final Pipe.SourceChannel userInput;
     public NonBlockingChatter(SocketChannel chatServerChannel,
                               Pipe.SourceChannel userInput) {
         // TODO
+        this.chatServerChannel = chatServerChannel;
+        this.userInput = userInput;
+        readBuffer = ByteBuffer.allocate(READ_BUFFER_SIZE);
+        writeBuffer = ByteBuffer.allocate(READ_BUFFER_SIZE);
+
     }
 
     @Override public void run() {
         // TODO
-    }
+        while(!Thread.currentThread().isInterrupted()){
+        	try{
+        		int len  = userInput.read(writeBuffer);
+        		writeBuffer.flip();
+        		chatServerChannel.write(writeBuffer);
+        		writeBuffer.clear();
+       
+            	int result = chatServerChannel.read(readBuffer);
+            	if(result<1){
+            		continue;
+            	}
+            	readBuffer.flip();
+            	CharsetDecoder decoder = UTF8.newDecoder();
+            	CharBuffer charBuffer = decoder.decode(readBuffer);
+            	readBuffer.clear();
+            	System.out.printf("%s%n", charBuffer.toString());
+            	
+        	}
+        	catch(Exception e){
+        		System.out.printf("Exception - %s%n", e.getMessage());
+        	}
 
+        }
+        
+
+    }
+    
 }
